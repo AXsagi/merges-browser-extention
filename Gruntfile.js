693b6c99ad38a801b6c35ac094060d5e4f357710
Sagi Haas <sagi.haas@axonius.com>
2022-08-29 21:24:29 +0300

    add automation to build

---
+++ Gruntfile.js
@@ -0,0 +1,224 @@
+/*jshint camelcase: false*/
+// Generated on 2014-02-15 using generator-chrome-extension 0.2.5
+'use strict';
+var serveStatic = require('serve-static');
+var mountFolder = function (connect, dir) {
+  return serveStatic(require('path').resolve(dir));
+};
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // configurable paths
+  var yeomanConfig = {
+    app: 'app',
+    dist: 'dist'
+  };
+
+  grunt.initConfig({
+    yeoman: yeomanConfig,
+    watch: {
+      options: {
+        spawn: false
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      test: {
+        options: {
+          middleware: function (connect) {
+            return [
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'test')
+            ];
+          }
+        }
+      }
+    },
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= yeoman.dist %>/*',
+            '!<%= yeoman.dist %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= yeoman.app %>/scripts/{,*/}*.js',
+        'test/spec/{,*/}*.js'
+      ]
+    },
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>/images',
+          src: '{,*/}*.{png,jpg,jpeg}',
+          dest: '<%= yeoman.dist %>/images'
+        }]
+      }
+    },
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>/images',
+          src: '{,*/}*.svg',
+          dest: '<%= yeoman.dist %>/images'
+        }]
+      }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          '<%= yeoman.dist %>/styles/main.css': [
+            '.tmp/styles/{,*/}*.css',
+            '<%= yeoman.app %>/styles/{,*/}*.css',
+            '!<%= yeoman.app %>/styles/{background,contentscript,options}.css'
+          ],
+          '<%= yeoman.dist %>/styles/background.css': [
+            '<%= yeoman.app %>/styles/background.css'
+          ],
+          '<%= yeoman.dist %>/styles/contentscript.css': [
+            '<%= yeoman.app %>/styles/contentscript.css'
+          ],
+          '<%= yeoman.dist %>/styles/options.css': [
+            '<%= yeoman.app %>/styles/options.css'
+          ]
+        }
+      }
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          /*removeCommentsFromCDATA: true,
+           // https://github.com/yeoman/grunt-usemin/issues/44
+           //collapseWhitespace: true,
+           collapseBooleanAttributes: true,
+           removeAttributeQuotes: true,
+           removeRedundantAttributes: true,
+           useShortDoctype: true,
+           removeEmptyAttributes: true,
+           removeOptionalTags: true*/
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          src: '*.html',
+          dest: '<%= yeoman.dist %>'
+        }]
+      }
+    },
+    // Put files not handled in other tasks here
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '<%= yeoman.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            'images/{,*/}*.{webp,gif}',
+            '_locales/{,*/}*.json'
+          ]
+        }, {
+          expand: true,
+          cwd: '.tmp/images',
+          dest: '<%= yeoman.dist %>/images',
+          src: [
+            'generated/*'
+          ]
+        }, {
+          expand: true,
+          dot: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '<%= yeoman.dist %>',
+          src: [
+            'scripts/*.js'
+          ]
+        }]
+      }
+    },
+    concurrent: {
+      server: [
+      ],
+      test: [
+      ],
+      dist: [
+        'imagemin',
+        'svgmin',
+        'htmlmin'
+      ]
+    },
+    chromeManifest: {
+      dist: {
+        options: {
+          buildnumber: false,
+          background: {
+            target:'scripts/background.js'
+          }
+        },
+        src: '<%= yeoman.app %>',
+        dest: '<%= yeoman.dist %>'
+      }
+    },
+    compress: {
+      dist: {
+        options: {
+          archive: 'package/do-not-merge-non-approved-prs.zip'
+        },
+        files: [{
+          expand: true,
+          cwd: 'dist/',
+          src: ['**'],
+          dest: ''
+        }]
+      }
+    }
+  });
+
+  grunt.registerTask('test', [
+    'clean:server',
+    'concurrent:test',
+    'connect:test'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'chromeManifest:dist',
+    'concurrent:dist',
+    'cssmin',
+    'copy',
+    'compress'
+  ]);
+
+  grunt.registerTask('default', [
+    'jshint',
+    'test',
+    'build'
+  ]);
+};
